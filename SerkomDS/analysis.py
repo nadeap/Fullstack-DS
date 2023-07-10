9c928f97d93b7aef4d7bfd283befd6fc1de359b9
wertax0 <nadeaaap@gmail.com>
2023-07-10 20:50:01 +0700

    fix analysis

--- SerkomDS/analysis.py
+++ SerkomDS/analysis.py
@@ -18,6 +18,8 @@ data.nunique()
 data.unique()
 data['kolom'].value_counts()
 data.duplicated().sum()
+data.drop_duplicates()
+data.fillna(axis=0) #0 row, 1 column
 
 ### Matplotlib
 #histogram hanya untuk 1 kolom
@@ -27,4 +29,43 @@ plt.xlabel('Nama Label X')
 plt.ylabel('Nama Label Y')
 plt.show()
 
+#Boxplot untuk 1 kolom
+data.boxplot(column='namakolom')
+plt.title('Judul Grafik')
+plt.ylabel('Nama Label Y')
+plt.show()
+
+#Scatter plot 2 kolom
+data.plot.scatter(x='kolomx', y='kolomy')
+plt.title()
+plt.xlabel()
+plt.ylabel()
+plt.show()
+
+#Barplot 2 kolom
+plt.bar(data['kolomA'], data['kolomB'])
+plt.title()
+plt.xlabel()
+plt.ylabel()
+plt.show()
+
+###Seaborn
+sns.scatterplot(x,y)
+sns.lineplot(x,y)
+sns.barplot(x,y)
+sns.histplot(x,y)
+sns.boxplot(x,y)
+sns.pairplot(data, hue='species')
 sns.heatmap(data=data.corr())
+
+#Pandas Cleaning Data
+data = data.drop(column='namakolom')
+data['kolomA'] = data['kolomA'].replace('KataAwal','KataGanti')
+
+###Drop Outliers
+Q1 = data['A'].quantile(0.25)
+Q3 = data['A'].quantile(0.75)
+IQR = Q3 - Q1
+lower_bound = Q1 - 1.5 * IQR
+upper_bound = Q3 + 1.5 * IQR
+filtered_data = data[(data['A'] >= lower_bound) & (data['A'] <= upper_bound)]
